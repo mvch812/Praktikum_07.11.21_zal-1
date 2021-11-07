cc492954e513fb1965a95d0d5e331bdaa6bf7c4e
MVCH812 <mvch812@gmail.com>
2021-11-07 14:13:25 +0300

    Решена задача 18, но не запускается

---
+++ Task_18/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y 
+
+bool x = true;
+bool y = false;
+bool istin(bool x, bool y);
+{
+    return (!(x || y)) == (!x && !y);
+}
+Console.WriteLine($"Истинно ли утверждение, что ¬(X ⋁ Y) = ¬X ⋀ ¬Y? Ответ: {istin(x, y)}");
