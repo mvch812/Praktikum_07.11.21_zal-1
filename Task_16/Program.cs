ec1bad59645e3f44301d1da34d9e3b40096b7e75
MVCH812 <mvch812@gmail.com>
2021-11-07 13:31:01 +0300

    Решена задача 16

---
+++ Task_16/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 16: Дано число обозначающее день недели. Выяснить является номер дня недели выходным  
+
+int i = new Random().Next(1, 8);
+bool weekEnd(int numb)
+{
+    return numb == 6 | numb == 7;
+}
+Console.WriteLine($"{i}-й день недели выходной? Ответ: {weekEnd(i)}");
