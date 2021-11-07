c984fc569c3c2170d0f1438908d546a21d2f3737
MVCH812 <mvch812@gmail.com>
2021-11-07 13:42:58 +0300

    Решена задача 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,32 @@
+﻿//19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0 
+
+int x = -90;
+int y = -9;
+int Chetvert(int x, int y)
+{
+    int nomerCh = 0;
+    if (x > 0)
+    {
+        if (y > 0)
+        {
+            nomerCh = 1;
+        }
+        else
+        {
+            nomerCh = 4;
+        }
+    }
+    else
+    {
+        if (y > 0)
+        {
+            nomerCh = 2;
+        }
+        else
+        {
+            nomerCh = 3;
+        }
+    }
+    return nomerCh;
+}
+Console.WriteLine($"Координата ({x},{y}) находится в {Chetvert(x, y)}-й четверти");
