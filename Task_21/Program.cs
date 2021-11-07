bdf034249a75513a7cc6f59aae88ea7850dec67b
MVCH812 <mvch812@gmail.com>
2021-11-07 14:35:31 +0300

    Решена задача 21

---
+++ Task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 21: Написать программу, которая проверяет пятизначное число на палиндром.
+
+Console.WriteLine("Введите пятизначное число:");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool Polindrom(int n)
+{
+    return (((n / 10000) == n % 10) && ((n / 1000 % 10) == (n / 10 % 10)));
+}
+Console.WriteLine($"Введенное число полиндром? Ответ: {Polindrom(n)}");
+
+// int m1 = (n / 10000);
+// int m2 = n % 10;
+// int m3 = (n / 1000 % 10);
+// int m4 = (n / 10 % 10);
+// Console.WriteLine($"{m1} {m2} {m3} {m4}");
