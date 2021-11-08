cfb0e93eba204ab9326abcd22f7f81e0960e9e8e
MVCH812 <mvch812@gmail.com>
2021-11-08 22:25:45 +0300

    Задача 287 решена

---
+++ Task_28/Program.cs
@@ -0,0 +1,24 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число:");
+
+long n = Convert.ToInt64(Console.ReadLine());
+
+long razryadnost(long n)
+{
+    long razr = 0;
+    long s = Math.Abs(n);
+    long p = 0;
+    if (n == 0)
+    {
+        return 0;
+    }
+    while (s >= 1)
+    {
+        p = p + s % 10;
+        s = s / 10;
+    }
+    return p;
+}
+
+Console.WriteLine($"Сумма цифр в указанном числе равна {razryadnost(n)}");
