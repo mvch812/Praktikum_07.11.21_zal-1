dcdc1f900b14308d84931f040eda9dc1a2f93783
MVCH812 <mvch812@gmail.com>
2021-11-08 21:09:39 +0300

    Задача 26 решена

---
+++ Task_26/Program.cs
@@ -0,0 +1,23 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число, которое необходимо возвести в степень:");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень возведения:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+double stepen(double n, double m)
+{
+    double st = n;
+    for (int x = 1; x < m; x++)
+    {
+        st = st * n;
+    }
+    return st;
+}
+
+Console.WriteLine($"Число {n} в степени {m} = {stepen(n, m)}");
+
+
+
