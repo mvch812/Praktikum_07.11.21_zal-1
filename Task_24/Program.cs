4d6c8b2d68fdbfc0165ec11409879c35dc6e3b31
MVCH812 <mvch812@gmail.com>
2021-11-08 20:20:43 +0300

    Задача 24 решена

---
+++ Task_24/Program.cs
@@ -0,0 +1,17 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+double i = new Random().Next(1, 100);
+
+void kub(double i)
+{
+    double kubb = 0;
+    for (int x = 1; x <= i; x++)
+    {
+        kubb = Math.Pow(x, 3);
+        Console.WriteLine($"Куб числа {x} = {kubb}");
+    }
+}
+
+kub(i);
+
+
