b3d90e0ac3f50d8e3728359118e7c0bb84ae1d1f
MVCH812 <mvch812@gmail.com>
2021-11-08 22:10:05 +0300

    Задача 27 решена

---
+++ Task_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.WriteLine("Введите число:");
+
+long n = Convert.ToInt64(Console.ReadLine());
+
+long razryadnost(long n)
+{
+    long razr = 0;
+    long s = Math.Abs(n);
+    if (n == 0)
+    {
+        return 1;
+    }
+    while (s >= 1)
+    {
+        s = s / 10;
+        razr++;
+    }
+    return razr;
+}
+
+Console.WriteLine($"В указанном числе {razryadnost(n)} цифр");
