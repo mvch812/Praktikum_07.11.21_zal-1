bcc3d9f2592f8ab8ef196f4fcb5f2c585e04a4ea
MVCH812 <mvch812@gmail.com>
2021-11-08 19:56:44 +0300

    Задача 25 решена

---
+++ Task_25/Program.cs
@@ -0,0 +1,14 @@
+﻿// 25. Найти сумму чисел от 1 до А
+int i = new Random().Next(1, 100);
+
+int summa(int i)
+{
+    int sum = 0;
+    for (int x = 1; x <= i; x++)
+    {
+        sum = sum + x;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {i} = {summa(i)}");
