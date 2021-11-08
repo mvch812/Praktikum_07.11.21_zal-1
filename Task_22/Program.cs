c03eca447248eb94e2c623e3b5e14686d0a75631
MVCH812 <mvch812@gmail.com>
2021-11-08 13:57:19 +0300

    Задача 22 решена

--- Task_22/Program.cs
+++ Task_22/Program.cs
@@ -1,33 +1,24 @@
 ﻿//Задача 22. Найти расстояние между точками в пространстве 2D/3D
 
+//Расчет расстояния для двухмерного пространства:
+
 double x1 = 1;
 double y1 = 3;
 double x2 = -3;
 double y2 = -1;
 
-// double r = Math.Pow(Math.Abs(x2 - x1), 2);
-// double p = Math.Pow(Math.Abs(y2 - y1), 2);
-// Console.WriteLine(Math.Pow(r,2));
-// Console.WriteLine(Math.Pow(p,2));
-// double rq= Math.Sqrt(Math.Pow(r,2)+Math.Pow(p,2));
-// Console.WriteLine(rq);
-
-double Rasst(double x1, double y1, double x2, double y2)
+double Rasst2D(double x1, double y1, double x2, double y2)
 {
-    double r = Math.Sqrt(Math.Pow((Math.Abs(x2 - x1)), 2) + Math.Pow((Math.Abs(y2 - y1)), 2));
-    return r;
+    return Math.Sqrt(Math.Pow((Math.Abs(x2 - x1)), 2) + Math.Pow((Math.Abs(y2 - y1)), 2));
 }
-Console.WriteLine($"Расстояние между точками ({x1},{y1}) и ({x2},{y2}) в пространстве 2D равно: {Rasst(x1, y1, x2, y2)}");
+Console.WriteLine($"Расстояние между точками ({x1},{y1}) и ({x2},{y2}) в пространстве 2D равно: {Rasst2D(x1, y1, x2, y2)}");
 
-// double r = Math.Round(Math.Sqrt(Math.Pow((Math.Abs(x2 - x1)), 2) + Math.Pow((Math.Abs(y2 - y1)), 2)));
-// Console.WriteLine(r);
-// int x1 = 98;
-// int y1 = 23;
-// int x2 = 8;
-// int y2 = 130;
+//Расчет расстояния для трехмерного пространства:
 
-// int Rasst(int x1, int y1, int x2, int y2);
-// {
-//     return Math.Sqrt(Math.Pow(Math.Abs(x2 - x1), 2) + Math.Pow(Math.Abs(y2 - y1), 2));
-// }
-// Console.WriteLine($"Расстояние между точками ({x1},{y1}) и ({x2},{y2}) в пространстве 2D равно: {Rasst(x1, y1, x2, y2)}");
+double z2 = -98;
+double z1 = 87;
+double Rasst3D(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow((Math.Abs(x2 - x1)), 2) + Math.Pow((Math.Abs(y2 - y1)), 2) + Math.Pow((Math.Abs(z2 - z1)), 2));
+}
+Console.WriteLine($"Расстояние между точками ({x1},{y1},{z1}), ({x2},{y2},{z2}) в пространстве 3D равно: {Rasst3D(x1, y1, x2, y2, z1, z2)}");
