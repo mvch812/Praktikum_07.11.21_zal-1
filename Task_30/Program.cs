6589d6b22d1ddebf45f00abe3e258be20ca7a764
MVCH812 <mvch812@gmail.com>
2021-11-09 10:36:28 +0300

    Задача 30 решена

---
+++ Task_30/Program.cs
@@ -0,0 +1,24 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число:");
+
+long i = Convert.ToInt64(Console.ReadLine());
+
+void kub(long i)
+{
+    double kubb = 0;
+    for (int x = 1; x <= i; x++)
+    {
+        if ((x % 10) % 2 == 0)
+        {
+            if ((x % 10) != 0)
+            {
+                kubb = Math.Pow(x, 3);
+                Console.WriteLine($"Куб числа {x} = {kubb}");
+            }
+        }
+    }
+}
+
+kub(i);
+
