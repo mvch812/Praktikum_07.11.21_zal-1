af73aa819e96c53482c1f05aab42a039a28f6f0e
MVCH812 <mvch812@gmail.com>
2021-11-09 08:45:54 +0300

    Задача 29 решена

---
+++ Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число:");
+
+long i = Convert.ToInt64(Console.ReadLine());
+
+long proizv(long i)
+{
+    long pr = 1;
+    for (long x = 1; x <= i; x++)
+    {
+        pr = pr * x;
+        Console.WriteLine(pr);
+    }
+    return pr;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {i} = {proizv(i)}");
+
