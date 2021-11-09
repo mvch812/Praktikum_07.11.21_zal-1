7a5446e3dad514c71e867d5084dae5a6220eb7ac
MVCH812 <mvch812@gmail.com>
2021-11-09 16:38:15 +0300

    Задача 39 решена

---
+++ Task_39/Program.cs
@@ -0,0 +1,46 @@
+﻿// 39.Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.WriteLine();
+void FillArrayA(int[] A) // Метод, заполняющий массив A случайными двузначными целыми числами от -10 до 10
+{
+    int length = A.Length;
+    int index = 0;
+    int summ = 0;
+    while (index < length)
+    {
+        A[index] = new Random().Next(-10, 10);
+        summ = summ + A[index];
+        index++;
+    }
+}
+int[] A = new int[17]; // задаем количество элементов массива A
+Console.WriteLine("Случайный массив:");
+FillArrayA(A);
+
+PrintArray(A);
+
+void PrintArray(int[] A) // Метод, печатающий массив A  
+{
+    int index = 0;
+    while (index < A.Length)
+    {
+        {
+            Console.Write(A[index] + " ");
+        }
+        index++;
+    }
+}
+
+Console.WriteLine();
+
+void Proizv(int[] A) // Метод, подсчитывающий произведение пар чисел в массиве
+{
+    int cur = 0;
+    for (int x = 0; x <= 6; x++)
+    {
+        cur = A[(A.Length - x - 1)] * A[x];
+        Console.WriteLine($"Произведение {x}-й пары чисел массива (первый-последний) равно: {cur}");
+    }
+}
+
+Proizv(A);
