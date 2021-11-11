f0f2133573a26a35358e6f6f3d294aebae80fed5
MVCH812 <mvch812@gmail.com>
2021-11-11 19:36:23 +0300

    Задача 45 решена

---
+++ Task_45/Program.cs
@@ -0,0 +1,32 @@
+﻿// 45. Показать числа Фибоначчи
+
+Console.WriteLine($"Последовательность Фебоначчи:");
+
+
+void FillArrayA(long[] A) // Метод, заполняющий массив A случайными двузначными целыми числами от -20 до 20
+{
+    A[0] = 0;
+    A[1] = 1;
+    for (int index = 2; index < A.Length; index++)
+    {
+        A[index] = A[index - 1] + A[index - 2];
+    }
+}
+long[] A = new long[50]; // задаем количество элементов массива A
+
+FillArrayA(A);
+
+
+
+void PrintArray(long[] A) // Метод, печатающий массив A  
+{
+    int index = 0;
+    while (index < A.Length)
+    {
+        {
+            Console.WriteLine($"{A[index]} ");
+        }
+        index++;
+    }
+}
+PrintArray(A);
