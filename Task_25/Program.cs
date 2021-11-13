fa4cc510aace1e5a41bd734684cd4cff72327d31
MVCH812 <mvch812@gmail.com>
2021-11-13 20:17:40 +0300

    Оптимизировали решение через формулу суммы первых n членов арифметической прогрессии

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,10 +1,15 @@
 ﻿// 25. Найти сумму чисел от 1 до А
 
+// ряд натуральных чисел образует арифметическую прогрессию с шагом 1.
+// Сумма первых n членов арифметической прогрессии определяется формулой:
+// S = (a1+an)*n/2;
+// a1 = 1, а значит S = (n+1)*n/2
+
 Console.WriteLine("Введите число:");
 
 long i = Convert.ToInt64(Console.ReadLine());
 
-long summa(long i)
+long summa1(long i) // не оптимальный метод
 {
     long sum = 0;
     for (long x = 1; x <= i; x++)
@@ -14,4 +19,18 @@ long summa(long i)
     return sum;
 }
 
-Console.WriteLine($"Сумма чисел от 1 до {i} = {summa(i)}");
+
+
+long summa2(long i) // оптимальный метод через формулу
+{
+    return ((i + 1) * i) / 2;
+}
+
+// Console.WriteLine($"Сумма чисел от 1 до {i} = {summa1(i)}");
+
+DateTime dt = DateTime.Now; // записываем текущее время до выполнения метода
+Console.WriteLine($"Сумма чисел от 1 до {i}  = {summa1(i)}");
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds); // считаем время выполнения операции в милисекундах
+dt = DateTime.Now;
+Console.WriteLine($"Сумма чисел от 1 до {i}  = {summa2(i)}");
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
