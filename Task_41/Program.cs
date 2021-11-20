2ea030528a4cf77754a32c4b8d57b62566556bc7
MVCH812 <mvch812@gmail.com>
2021-11-20 07:23:38 +0300

    Задача 41  решена

---
+++ Task_41/Program.cs
@@ -0,0 +1,21 @@
+﻿//Выяснить являются ли три числа сторонами треугольника 
+
+
+Console.WriteLine("Введите число №1:");
+
+long a = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine("Введите число №2:");
+
+long b = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine("Введите число №3:");
+
+long c = Convert.ToInt64(Console.ReadLine());
+
+bool triangle(long a, long b, long c)
+{
+    return ((a < b + c) && (b < a + c) && (c < a + b));
+}
+
+Console.WriteLine($"Числа {a}, {b}, {c} являются сторонами треугольника? Ответ:{triangle(a, b, c)}");
