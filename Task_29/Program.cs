1de3521d591b6a23e1047236b717c582f5450c9d
MVCH812 <mvch812@gmail.com>
2021-11-23 13:09:24 +0300

    Добавлено решение через рекурсию

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,19 +1,32 @@
-﻿// 29. Написать программу вычисления произведения чисел от 1 до N (т.е. факториал)
+﻿// // 29. Написать программу вычисления произведения чисел от 1 до N (т.е. факториал)
 
 Console.WriteLine("Введите число:");
 
 long i = Convert.ToInt64(Console.ReadLine());
 
-long proizv(long i)
+//Решение 1:
+// long proizv(long i)
+// {
+//     long pr = 1;
+//     for (long x = 1; x <= i; x++)
+//     {
+//         pr = pr * x;
+//         Console.WriteLine(pr);
+//     }
+//     return pr;
+// }
+// DateTime dt = DateTime.Now; // записываем текущее время до выполнения метода
+// Console.WriteLine($"Произведение чисел от 1 до {i} = {proizv(i)}");
+// Console.WriteLine((DateTime.Now - dt).TotalMilliseconds); // считаем время выполнения операции в милисекундах
+
+// // Решение через рекурсию:
+long Factorial (long i)
 {
-    long pr = 1;
-    for (long x = 1; x <= i; x++)
-    {
-        pr = pr * x;
-        Console.WriteLine(pr);
-    }
-    return pr;
+    // 1!=1 факториал 1 = 1
+    //0!=1 факториал 0=1
+    if (i==1) return 1;
+    else return i*Factorial(i-1);
 }
-
-Console.WriteLine($"Произведение чисел от 1 до {i} = {proizv(i)}");
-
+DateTime dt = DateTime.Now; // записываем текущее время до выполнения метода
+Console.WriteLine($"Произведение чисел от 1 до {i} = {Factorial(i)}");
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds); // считаем время выполнения операции в милисекундах
