f10917f347f3b0302d21b51fb0687baa42ea5421
MVCH812 <mvch812@gmail.com>
2021-11-23 15:48:11 +0300

    Задача 48 решена

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -1,11 +1,39 @@
-﻿// Найдите сумму натуральных чисел кратных 3 или 5 ниже 1000.
+﻿// 48.	Показать двумерный массив размером m×n заполненный целыми числами
 
-int sum = 0;
-for (int x = 1; x < 1000; x++)
+Console.WriteLine("введите размер массива (количество строк):");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите размер массива (количество столбцов):");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] Array = new int[m, n]; // выделяем память под массив (вводим массив)
+
+void fillArray(int m, int n, int[,] Array) // метод, заполняющий массив случайными числами
 {
-    if (x % 3 == 0 | x % 5 == 0)
+    for (int i = 0; i < Array.GetLength(0); i++)
     {
-        sum = sum + x;
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(-10, 10); //параметр случайных чисел от.. до ..
+        }
     }
 }
-Console.WriteLine($"Сумма натуральных чисел кратных 3 или 5 ниже 1000 равна:{sum}");
+
+fillArray(m, n, Array); // заполняем массив
+
+string PrintArray(int[,] Array)  // метод печатающий массив в строковую переменную
+{
+    string print = string.Empty;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            print = print + Array[i, j] + ' ';
+        }
+        print = print + '\n';
+    }
+    return print;
+}
+Console.WriteLine();
+Console.WriteLine($"Двумерный массив размером {m}*{n} заполненный целыми числами:");
+Console.Write(PrintArray(Array)); // печатаем строковую переменную с массивом
