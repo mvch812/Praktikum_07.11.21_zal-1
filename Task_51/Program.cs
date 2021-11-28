3728cfde9035f82734aca4672408890373cd57dc
MVCH812 <mvch812@gmail.com>
2021-11-28 11:01:01 +0300

    Задача 51 решена

---
+++ Task_51/Program.cs
@@ -0,0 +1,40 @@
+﻿// 51.	Задать двумерный массив следующим правилом: Aₘₙ = m+n
+Console.WriteLine("Введите размер массива:");
+
+Console.WriteLine("количество строк:");
+int arraySize0 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("количество столбцов:");
+int arraySize1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateArray(int arraySize0, int arraySize1) // метод, создающий массив
+{
+    int[,] Array = new int[arraySize0, arraySize1];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = i + j; //параметр случайных чисел от.. до ..
+        }
+    }
+    return Array;
+}
+
+int[,] Array = CreateArray(arraySize0, arraySize1);
+
+string PrintArray(int[,] Array)  // метод печатающий массив в строковую переменную
+{
+    string print = string.Empty;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            print = print + $"{Array[i, j],4} ";
+        }
+        print = print + '\n';
+    }
+    return print;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Двумерный массив размером {arraySize0}*{arraySize1} заполненный целыми числами:");
+Console.Write(PrintArray(Array)); // печатаем строковую переменную с массивом
