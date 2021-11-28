d4ec9e0bfc19c154b1540975c2fb90577cac9b18
MVCH812 <mvch812@gmail.com>
2021-11-28 10:30:24 +0300

    Оптимизирован код

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -1,39 +1,38 @@
 ﻿// 48.	Показать двумерный массив размером m×n заполненный целыми числами
 
 Console.WriteLine("введите размер массива (количество строк):");
-int m = Convert.ToInt32(Console.ReadLine());
+int arraySize0 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите размер массива (количество столбцов):");
-int n = Convert.ToInt32(Console.ReadLine());
+int arraySize1 = Convert.ToInt32(Console.ReadLine());
 
-int[,] Array = new int[m, n]; // выделяем память под массив (вводим массив)
-
-void fillArray(int[,] Array) // метод, заполняющий массив случайными числами
+int[,] CreateArray(int arraySize0, int arraySize1, int minVlue, int maxValue) // метод, создающий массив, заполненный случайными числами
 {
+    int[,] Array = new int[arraySize0, arraySize1];
     for (int i = 0; i < Array.GetLength(0); i++)
     {
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-            Array[i, j] = new Random().Next(-10, 10); //параметр случайных чисел от.. до ..
+            Array[i, j] = new Random().Next(minVlue, maxValue);
         }
     }
+    return Array;
 }
 
-fillArray(Array); // заполняем массив
+int[,] Array = CreateArray(arraySize0, arraySize1, -100, 100);
 
 string PrintArray(int[,] Array)  // метод печатающий массив в строковую переменную
 {
     string print = string.Empty;
     for (int i = 0; i < Array.GetLength(0); i++)
     {
-
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-            print = print + Array[i, j] + ' ';
+            print += $"{Array[i, j],4} ";
         }
         print = print + '\n';
     }
     return print;
 }
 Console.WriteLine();
-Console.WriteLine($"Двумерный массив размером {m}*{n} заполненный целыми числами:");
+Console.WriteLine($"Двумерный массив размером {arraySize0}*{arraySize1} заполненный целыми числами:");
 Console.Write(PrintArray(Array)); // печатаем строковую переменную с массивом
