2668ef9e42661471539f2dd28c670a6558f64068
MVCH812 <mvch812@gmail.com>
2021-12-04 12:49:19 +0300

    Задача 67 решена

---
+++ Task_67/Program.cs
@@ -0,0 +1,9 @@
+﻿// 67.	Показать натуральные числа от N до 1, N задано
+
+string PrintNumbersNto1(int n)
+{
+    if (n == 1) return "1";
+    else return $"{n} {PrintNumbersNto1(n-1)}";
+}
+
+Console.WriteLine(PrintNumbersNto1(30));
