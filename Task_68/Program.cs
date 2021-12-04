1f50345f4781d7ae32f7f400f8bce08fd451574a
MVCH812 <mvch812@gmail.com>
2021-12-04 09:50:16 +0300

    Задача 68  решена

---
+++ Task_68/Program.cs
@@ -0,0 +1,10 @@
+﻿// 68.	Показать натуральные числа от M до N, N и M заданы
+
+int PrintNumbersNtoM(int i, int n)
+{
+    Console.Write($"{i} ");
+    if (i == n) return n;
+    return PrintNumbersNtoM(i + 1, n);
+}
+
+PrintNumbersNtoM(7, 70);
