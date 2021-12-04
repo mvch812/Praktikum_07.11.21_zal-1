e9e0aec35a635cdf86348dc598435fc4305b109a
MVCH812 <mvch812@gmail.com>
2021-12-04 12:43:37 +0300

    Решение переписано

---
+++ Task_66/Program.cs
@@ -0,0 +1,10 @@
+﻿// 66.	Показать натуральные числа от 1 до N, N задано
+
+string PrintNumbers1toN(int n)
+{
+    if (n == 1) return "1";
+    else return $"{PrintNumbers1toN(n - 1)} {n}";
+}
+
+Console.WriteLine(PrintNumbers1toN(50));
+
--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -1,10 +1,16 @@
 ﻿// 68.	Показать натуральные числа от M до N, N и M заданы
 
+
 int PrintNumbersNtoM(int i, int n)
 {
     Console.Write($"{i} ");
     if (i == n) return n;
-    return PrintNumbersNtoM(i + 1, n);
+    else
+    {
+        if (i > n) return PrintNumbersNtoM(i - 1, n);
+        else return PrintNumbersNtoM(i + 1, n);
+    }
 }
+PrintNumbersNtoM(10, 100);
 
-PrintNumbersNtoM(7, 70);
+
