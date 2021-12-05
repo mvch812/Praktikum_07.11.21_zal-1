8604347df454b73808807515afa957bdda221b36
MVCH812 <mvch812@gmail.com>
2021-12-05 11:47:39 +0300

    Задача 23  решена

---
+++ Task_23/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// 23. Показать таблицу квадратов чисел от 1 до N 
+
+Console.WriteLine("Введите число:");
+
+long i = Convert.ToInt64(Console.ReadLine());
+void quadr(double i)
+{
+    Console.WriteLine($"| число | квадрат числа |");
+    Console.WriteLine($"_________________________");
+    double q = 0;
+    for (int x = 1; x <= i; x++)
+    {
+        q = Math.Pow(x, 2);
+        Console.WriteLine($"|{x,6} | {q,14} |");
+    }
+}
+quadr(i);
+
