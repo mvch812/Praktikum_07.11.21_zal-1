ee5fff852a79ad9a9a62eb1536fb38e48c89ba81
MVCH812 <mvch812@gmail.com>
2021-12-05 09:48:24 +0300

    дописано для отрицательных

--- Task_70/Program.cs
+++ Task_70/Program.cs
@@ -1,8 +1,8 @@
 ﻿// 70.	Найти сумму цифр числа
-int n = 890;
+int n = -3095;
 int SummNumberElements(int n, int div = 1)
 {
     if (n / div == 0) return 0;
-    else return ((n / div) % 10) + SummNumberElements(n, div * 10);
+    else return ((Math.Abs(n / div) % 10)) + SummNumberElements(n, div * 10);
 }
 Console.WriteLine($"Сумма цифр числа {n} = {SummNumberElements(n)} ");
