deeb97174c2bf903bf8d4213819f02d372a83459
MVCH812 <mvch812@gmail.com>
2022-01-23 16:01:43 +0300

    Задача 11  решена

---
+++ Task_11/Program.cs
@@ -0,0 +1,21 @@
+﻿// 11.	Показать вторую цифру трёхзначного числа
+
+int TwoNumber(int n)
+{
+    return (n % 100)/10;
+}
+
+int InputNumber()
+{
+    Console.WriteLine("Введите трехзначное число:");
+    while (true)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n > 99 && n < 1000) return n;
+        else Console.WriteLine("Трехзначное число не введено. Повторите ввод:");
+    }
+}
+int m = InputNumber();
+
+Console.WriteLine($"Вторая цифра введенного числа:{TwoNumber(m)}");
+
