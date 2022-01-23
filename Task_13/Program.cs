ad649ba0efd824b12b3f4bcbce2efa75a5283c95
MVCH812 <mvch812@gmail.com>
2022-01-23 16:29:07 +0300

    Task 13 solved

---
+++ Task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// 13.	Удалить вторую цифру трёхзначного числа
+
+int DelTwoNumber(int n)
+{
+    return (n / 100) * 10 + n % 10;
+}
+
+int InputNumber()
+{
+    Console.WriteLine("Введите трехзначное число:");
+    while (true)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n > 99 && n < 1000) return n;
+        else Console.WriteLine("Трехзначное число не введено. Повторите ввод:");
+    }
+}
+int m = InputNumber();
+
+Console.WriteLine($"Новое число:{DelTwoNumber(m)}");
+
