2434f9aed9c35b8770f46f60bde177ce22c65e00
MVCH812 <mvch812@gmail.com>
2022-01-23 20:33:39 +0300

    Task 09 solved

---
+++ Task_09/Program.cs
@@ -0,0 +1,15 @@
+﻿// 9.	Показать четные числа от 1 до N
+
+int n = 77;
+string numbers1toN(int n)
+{
+    string print = string.Empty;
+
+    for (int i = 2; i < n + 1; i++)
+    {
+        if (i % 2 == 0) print += $"{i} ";
+    }
+    return print;
+}
+
+Console.WriteLine(numbers1toN(n));
