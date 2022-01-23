c31b37eccf64ce2fea0ebf8d4f886b458a1c5887
MVCH812 <mvch812@gmail.com>
2022-01-23 20:08:03 +0300

    Task 03 solved

---
+++ Task_03/Program.cs
@@ -0,0 +1,12 @@
+﻿// 3.	Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+string MaxMinNumber(int a, int b)
+{
+    if (a > b) return $"Число {a} - большее, число {b} - меньшее";
+    else return $"Число {b} - большее, число {a} - меньшее";
+}
+Console.WriteLine(MaxMinNumber(a, b));
