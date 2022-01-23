5c1f9f7b4599a18990990dd2c2aaa5fad9ec8dd4
MVCH812 <mvch812@gmail.com>
2022-01-23 20:13:31 +0300

    Task 08 solved

---
+++ Task_08/Program.cs
@@ -0,0 +1,14 @@
+﻿// 8.	Показать числа от -N до N
+
+int n = 20;
+int[] array = new int[n * 2 + 1];
+array[0] = -n;
+for (int index = 1; index < array.Length; index++)
+{
+    array[index] = array[index - 1] + 1;
+}
+
+for (int index = 0; index < array.Length; index++)
+{
+    Console.Write($"{array[index]} ");
+}
