f897a8cf31770fa5d517b0c531be8cdf64569915
MVCH812 <mvch812@gmail.com>
2022-01-23 15:03:56 +0300

    Задача 10  решена

---
+++ Task_10/Program.cs
@@ -0,0 +1,21 @@
+﻿//10: Показать последнюю цифру трёхзначного числа
+
+int LastNumber(int n)
+{
+    return n % 10;
+}
+
+int InputNumber()
+{
+    Console.WriteLine("Введите трехзначное число:");
+    while (true)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n > 99 && n < 1000) return n;
+        else Console.WriteLine("Трехзначное число не введено. Повторите ввод:");
+    }
+}
+int m = InputNumber();
+
+Console.WriteLine($"Последняя цифра введенного числа:{LastNumber(m)}");
+
