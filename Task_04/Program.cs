13c89e1ef29cdf5c9ee3aa3e137fa811990b9e05
MVCH812 <mvch812@gmail.com>
2022-01-23 12:30:41 +0300

    Задача 4 решена

---
+++ Task_04/Program.cs
@@ -0,0 +1,22 @@
+﻿//4. По заданному номеру дня недели вывести его название
+
+void WeekDay(int dayNumber)
+{
+    String[] week = { "Мonday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+    Console.WriteLine(week[dayNumber - 1]);
+
+}
+
+
+Console.WriteLine("Введите день недели:");
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 7 && n >= 1)
+{
+    WeekDay(n);
+}
+else
+{
+    Console.WriteLine("Вы ошиблись, в неделе только 7 дней :)");
+}
